20b1f4972bbaaa5f883ca5ae8228419dfb4b75ee
skripter <g.doblesiete@gmail.com>
2025-06-18 22:10:15 -0300

    Add files via upload

--- veterinaria/mascotas-editar.php
+++ veterinaria/mascotas-editar.php
@@ -4,48 +4,54 @@
 require('./db.php');
 $msgok = "";
 $msgerror = "";
+$masid = 0;
 
-if(!$_GET){
+if(!$_GET and !$_POST){
 	header("Location: ./mascotas-listar.php");
 	exit("No hay datos");
 }
 
-$masid = $_GET['masid'];
-
 if($_POST){
 	extract($_POST,EXTR_OVERWRITE);
+	unset($msgerror);
 	//var_dump($_POST);
-	if(!is_numeric($petrace)){
-		$msgerror = "No se pudo guardar, raza incorrecta";
-	}
+	if(!is_numeric($masid) or $masid < 1){ $msgerror = "ID incorrecto"; }
+	if(!is_numeric($petrace)){ $msgerror = "No se pudo guardar, raza incorrecta"; }
 	/*if(!is_numeric($petowner)){
 		$msgerror = "No se pudo guardar, dueño incorrecto";
 	}*/
 	
 if(!isset($msgerror)){
+	$masid = mysqli_real_escape_string($conn, $masid);
 	$petname = mysqli_real_escape_string($conn, $petname);
 	//guardo en la DB
-	$sql = "INSERT INTO mascotas (masnombre, masespecie, masraza, massexo, masfechanac, maspropietario) ";
-	$sql .= "VALUES ('".$petname."', '".$petspecie."', '".$petrace."', '".$petsex."','".$petdob."', '".$petownername."')";
+	$sql = "UPDATE mascotas SET masnombre='".$petname."',  masespecie='".$petspecie."', masraza='".$petrace."', massexo='".$petsex."', masfechanac='".$petdob."', maspropietario='".$petownername."'";
+	$sql .= " WHERE masid='".$masid."'";
+	echo $sql;//exit();
 	$result = mysqli_query($conn, $sql);
+	$petname = stripslashes($petname);
 	$msgok = "Se guardó correctamente la mascota: ". $petname;
 }//if msgerror
 
 }//fin POST
 
 if($_GET){
+	$masid = $_GET['masid'];
+	if(!is_numeric($masid) or $masid < 1){
+		header("Location: ./mascotas-listar.php");
+		exit("No hay datos");
+	}//fin if $masid
 	$masid = mysqli_real_escape_string($conn,$masid);
 	$sql = "SELECT * FROM mascotas WHERE masid='".$masid."'";
 	//echo $sql;
 	$resultado = mysqli_query($conn,$sql);
 	while($row = mysqli_fetch_array($resultado, MYSQLI_ASSOC)){
-		$masnombre = htmlentities($row['masnombre']);
-		$masespecie = $row['masespecie'];
-		$masraza = $row['masraza'];
-		$massexo = $row['massexo'];
-		$masdueño = $row['maspropietario'];
-		$masnacimiento = $row['masfechanac'];
-		$maspropietario = $row['maspropietario'];
+		$petname = $row['masnombre'];
+		$petspecie = $row['masespecie'];
+		$petrace = $row['masraza'];
+		$petsex = $row['massexo'];
+		$petdob = $row['masfechanac'];
+		$petownername = $row['maspropietario'];
 	}//fin while
 }//fin GET
 ?>
@@ -71,6 +77,7 @@ if($_GET){
 	
 <article>
 <form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='POST'>
+<input type="hidden" name="masid" value='<?php echo $masid; ?>'>
 <?php
 echo "<h4>$msgerror</h4>";
 echo "<h4>$msgok</h4>";
@@ -80,7 +87,7 @@ echo "<h4>$msgok</h4>";
 	
 	<tr>
 	<td>Nombre:</td>
-	<td><input type='text' name='petname' id='petname' value='<?php echo $masnombre; ?>' autofocus required></td>
+	<td><input type='text' name='petname' id='petname' value='<?php echo htmlentities($petname); ?>' autofocus required></td>
 	</tr>
 	
 	<tr>
@@ -94,7 +101,7 @@ echo "<h4>$msgok</h4>";
 	$result = mysqli_query($conn, $sql);
 	while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
 		$selected = "";
-		if($row['espid'] == $masespecie){ $selected = " selected";}
+		if($row['espid'] == $petspecie){ $selected = " selected";}
 		echo "<option value='".$row['espid']."'".$selected.">";
 		echo $row['espid']." - ".$row['espnombre']."</option>\n";
 }//fin while
@@ -114,7 +121,7 @@ echo "<h4>$msgok</h4>";
 	$result = mysqli_query($conn, $sql);
 	while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
 		$selected = "";
-		if($row['razid'] == $masraza){ $selected = " selected";}
+		if($row['razid'] == $petrace){ $selected = " selected";}
 		echo "<option value='".$row['razid']."'".$selected.">";
 		echo $row['razid']." - ".$row['raznombre']."</option>\n";
 }//fin while
@@ -128,28 +135,28 @@ echo "<h4>$msgok</h4>";
 	<td>
 	<select name='petsex' id='petsex'>
 	<option value="">Seleccione sexo</option>
-	<option value="H" <?php if($massexo=="H"){ echo " selected";} ?>>Hembra</option>
-	<option value="M" <?php if($massexo=="M"){ echo " selected";} ?>>Macho</option>
+	<option value="H" <?php if($petsex=="H"){ echo " selected";} ?>>Hembra</option>
+	<option value="M" <?php if($petsex=="M"){ echo " selected";} ?>>Macho</option>
 	</select>
 	</td>
 	</tr>
 	
 	<tr>
 	<td>Fecha Nac.:</td>
-	<td><input type='date' name='petdob' id='petdob' value='<?php echo $masnacimiento; ?>' required></td>
+	<td><input type='date' name='petdob' id='petdob' value='<?php echo $petdob; ?>' required></td>
 	</tr>
 	
 	<tr>
 	<td>Dueño:</td>
 	<td>
 	<input type='text' name='petowner' id='petowner' readonly>
-	<input type='text' name='petownername' id='petownername' value='<?php echo $maspropietario; ?>'required>
+	<input type='text' name='petownername' id='petownername' value='<?php echo $petownername; ?>'required>
 	</td>
 	</tr>
 	
 	<tr>
 	<td><button type='reset'>Limpiar</button></td>
-	<td><button type='submit'>Guardar</button></td>
+	<td><button type='submit'>Editar</button></td>
 	</tr>
 	
 	</table>
