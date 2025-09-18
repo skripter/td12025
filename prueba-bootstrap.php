b21176eb2e0409530790685e458c06bbd0dff078
skripter <g.doblesiete@gmail.com>
2025-09-17 22:01:44 -0300

    Add files via upload

---
+++ prueba-bootstrap.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba bootstrap</title>
+    <link rel="stylesheet" href="/bootstrap-5.3.8-dist/css/bootstrap.css">
+    <script src="/js/jquery-3.7.1.min.js"></script>
+    <script>
+        $(function(){
+			
+        });//fin $
+    </script>
+</head>
+<body class="bg-light mt-1">
+<div class="container-fluid">
+<button class="btn btn-danger">Boton rojo</button>
+<button class="btn btn-primary">Boton azul</button>
+</div><!-- fin container -->
+</body>
+</html>
